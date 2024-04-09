ca904b394ea439f8886a4f081ffd2881d9268350
Jorge Castellon Jr <jorge@castellon.dev>
2024-04-08 17:49:49 -0700

    Adds skeleton for loading images

--- src/app/manga/[manga]/[chapter]/page.tsx
+++ src/app/manga/[manga]/[chapter]/page.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import { SingleChapter } from "@/app/api/manga/[id]/[chapter]/GetSingleChapter";
 import Image from "next/image";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function SingleGenre({
   params,
@@ -10,6 +11,8 @@ export default function SingleGenre({
 }) {
   const [loading, setLoading] = useState(true);
   const [chapter, setChapter] = useState<SingleChapter>();
+  const [imagesToLoad, setImagesToLoad] = useState<number>(0);
+  const [loadedImages, setLoadedImages] = useState<number>(0);
 
   // fetch the data from the api
   useEffect(() => {
@@ -17,27 +20,35 @@ export default function SingleGenre({
       const all = await fetch(`/api/manga/${params.manga}/${params.chapter}`);
       const allData = await all.json();
       setChapter(allData);
-      setLoading(false);
+      setImagesToLoad(allData.images.length);
     };
     fetchData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  useEffect(() => {
+    if (imagesToLoad === loadedImages) {
+      setLoading(false);
+    }
+  }, [loadedImages, imagesToLoad]);
+
+  const handleImageLoad = () => {
+    setLoadedImages((prev) => prev + 1);
+  };
+
   return (
     <>
-      {loading ? (
-        <div className="w-full h-screen grid items-center justify-center">
-          Loading
-        </div>
-      ) : (
-        chapter && (
-          <div className="grid gap-8 p-4">
-            <div>
-              <h1 className="text-4xl font-black">{chapter.mangaTitle}</h1>
-              <h2>{chapter.title}</h2>
-            </div>
-            <div className="-mx-4">
-              {chapter.images.map((image, index) => (
+      {chapter && (
+        <div className="grid gap-8 p-4">
+          <div>
+            <h1 className="text-4xl font-black">{chapter.mangaTitle}</h1>
+            <h2>{chapter.title}</h2>
+          </div>
+          <div className="-mx-4">
+            {loading ? (
+              <Skeleton className="h-[500px] w-full rounded-xl" />
+            ) : (
+              chapter.images.map((image, index) => (
                 <div key={image} className="relative w-full">
                   <Image
                     src={image}
@@ -45,13 +56,14 @@ export default function SingleGenre({
                     width={0}
                     height={0}
                     sizes="100vw"
-                    style={{ width: "100%", height: "auto" }} // optional
+                    style={{ width: "100%", height: "auto" }}
+                    onLoadingComplete={handleImageLoad}
                   />
                 </div>
-              ))}
-            </div>
+              ))
+            )}
           </div>
-        )
+        </div>
       )}
     </>
   );
